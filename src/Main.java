1261a76fd8b74c4030647828af23472068b61b8a
SonyOtto <o.m.shevelov@gmail.com>
2019-02-23 00:24:27 +0200

    Общее время - ОК Время по проекту - ОК Время по задаче1 - ОК Время по задаче2 - ОК

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
-        String text = "Добрый вечер! Всего за сегодня было отработано 8.5 ч. Задачи, выполненные за сегодня: 1. 888 Holdings - Poker - 6,5 ч. Task 1190615:[QA Mobile] Exploratory Testing 7.14 - Sit&Go - 3.5 ч. Gameplay HH Ad-";
+        String text = "Добрый вечер! Всего за сегодня было отработано 8.5 ч. Задачи, выполненные за сегодня: 1. 888 Holdings - Poker - 6,5 ч. Task 1190615:[QA Mobile] Exploratory Testing 7.14 - Sit&Go - 3.5 ч. Gameplay HH Ad- Task 1190586:[Mobile QA] Android wrapper (push fix) PP - Testlab Execution - 2,5 ч.";
         Pattern pattern1 = Pattern.compile("Всего за сегодня было отработано\\s[0-9]+([,.][0-9]{1,2})?");
         Matcher matcher = pattern1.matcher(text);
         while (matcher.find()) {
